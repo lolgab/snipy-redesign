de001cd64445a96df5c8bb35779c483b2d150f7a
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-04-02 19:01:46 +0200

    fix

--- snipy/src/main/scala/snipy/facades/builtins.scala
+++ snipy/src/main/scala/snipy/facades/builtins.scala
@@ -4,7 +4,7 @@ import snipy._
 import dynamic._
 
 object builtins {
-  private val builtins = module("builtins")(PyZone.leakingZone, di)
+  private val builtins = module("builtins")(PyZone.leakingZone, pyDynamic)
 
   def str(o: PyObject[PyObjectPtr]): String = PyZone { implicit z =>
     builtins.str(o).asInstanceOf[PyObject[PyStringPtr]].asScala[String]
